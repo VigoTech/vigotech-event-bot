6ff602c8a0a248621fed46ec14eb9e0828cdb8a3
Sergio Carracedo <info@sergiocarracedo.es>
2018-12-20 18:44:16 +0000

    upcoming.js edited online with Bitbucket

--- src/command/upcoming.js
+++ src/command/upcoming.js
@@ -10,14 +10,10 @@ function findUpcomingEvents(events, range) {
 module.exports = function(argv) {
     const events = require('../events').events();
     const tweet = require('../tweet');
-
     const upcommingEvents = findUpcomingEvents(events, parseInt(argv.minutes) + 1 );
-    
-    console.log(upcommingEvents)
-
-
+        
     for (let groupKey in upcommingEvents) {
-        let group = events[groupKey];
+        let group = upcommingEvents[groupKey];
         let eventDate = new Date(group.nextEvent.date);
         let eventTimeString = (eventDate.getHours() < 10 ? '0': '') + eventDate.getHours() + ':' + (eventDate.getMinutes() < 10 ? '0': '') + eventDate.getMinutes();
         let status = `O evento de ${group.name} (${group.nextEvent.title}) comeza as ${eventTimeString}. Máis info ${group.nextEvent.url} ou en http://vigotech.org`;
